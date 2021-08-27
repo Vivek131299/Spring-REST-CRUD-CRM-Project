566e1f32ba48ed44b67264bffbf7a39427e0fb10
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-27 17:57:25 +0530

    Get Single Customer

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,17 @@ public class CustomerRestController {
 		
 		// delegate the call to CustomerService
 		return customerService.getCustomers();
+	}
+	
+	
+	// Get Single Customer
+	// add mapping for GET /customers/{customerId}
+	// This will return the customer based on id that we pass if it matched with the id column in our database..
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		// delegate the call to CustomerService
+		Customer theCustomer = customerService.getCustomer(customerId);
+		return theCustomer;
 	}
 }
